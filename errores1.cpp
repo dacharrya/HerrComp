6523c26da42272c0aaa7562707ffc1770899bd70
Diego Charry <dacharrya@unal.edu.co>
2021-10-17 15:44:10 -0500

    Añadido errores1

---
+++ errores1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main () 
+{ 
+
+float xover= 1.23e40;
+float xunder = 1.23e-50;
+
+std::cout << "xover = " << xover << "\n";
+std::cout << "xunder = " << xunder << "\n";
+
+return 0;
+}
