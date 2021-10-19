eef64c168c04b4bc740d6abe6177b4355d0f0aaa
Diego Charry <dacharrya@unal.edu.co>
2021-10-19 12:06:06 -0500

    intento mil de la tarea aun no funciona

--- practicaerroresnumericos.cpp
+++ practicaerroresnumericos.cpp
@@ -1,7 +1,7 @@
 #include<cmath>
 #include <iostream>
 
-float fact(int a)
+double fact(int a)
 {
   int F = 1;
   for (int i=1; i <= a; i++)
@@ -16,12 +16,12 @@ double mysin( double x, int N)
   double seno = 0;
   for (int k=0; k < N ; k++)
     {
-      seno = seno + pow(-1,k)*pow(x, 2 * k + 1)/ fact(2 * k + 1);
+      seno = seno + pow(-1,k) * pow(x, 2 * k + 1)/ fact(2 * k + 1);
     }
   std::cout <<"\n"<< seno << std::endl;
  return 0;
 }
-
+ 
 int main ()
 {
   std::cout.setf(std::ios::scientific);
@@ -29,7 +29,7 @@ int main ()
   const double x = M_PI/3;
   const double exact = std::sin(x);
 
-  for(int NMAX = 1 ; NMAX <= 1000; NMAX++) {
+  for(int NMAX = 1 ; NMAX <= 10; NMAX++) {
     double diff = std::fabs(mysin(x, NMAX) - exact)/exact;
     std::cout << NMAX << "\t" << diff << "\n";
 
